da77d70947c27ce961f83941d3bf17c421f25155
Xenthio <thegooster88@gmail.com>
2022-05-18 08:49:49 +1000

    Add basic walk controller
    Add walk controller and renamed pawn into GamePlayer

---
+++ code/GamePlayer.cs
@@ -0,0 +1,48 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace Sandbox
+{
+	partial class GamePlayer : Player
+	{
+		/// <summary>
+		/// Called when the entity is first created 
+		/// </summary>
+		/// 
+		public Clothing.Container Clothing = new();
+		public override void Spawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+			Controller = new WalkController();
+
+			Animator = new StandardPlayerAnimator();
+			CameraMode = new FirstPersonCamera();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			Clothing.DressEntity( this );
+			base.Respawn();
+		}
+
+		/// <summary>
+		/// Called every tick, clientside and serverside.
+		/// </summary>
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+		}
+
+		public override void FrameSimulate( Client cl )
+		{
+			base.FrameSimulate( cl );
+
+			// Update rotation every frame, to keep things smooth
+			Rotation = Input.Rotation;
+			EyeRotation = Rotation;
+		}
+	}
+}
