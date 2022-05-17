c901d787e7881a363a74f80768c254868a343367
Xenthio <thegooster88@gmail.com>
2022-05-18 09:33:35 +1000

    Seperate these now instead of later

--- code/Collectable.cs
+++ code/Collectable.cs
@@ -1,14 +1,4 @@
-﻿using Sandbox;
-using System;
-using System.Linq;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
-using Hammer;
-
-[Library( "collectable_base" )]
-[Display( Name = "Collectable" ), Category( "Collectables" ), Icon( "place" )]
-// Base collectable, for testing everything is here now, seperate stuff later.
-public partial class Collectable : ModelEntity
+﻿public partial class Collectable : ModelEntity
 {
 	
 }
---
+++ code/Collectables/TestCollectable.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Hammer;
+
+[Library( "collectable_test" )]
+[Display( Name = "Test Collectable" ), Category( "Collectables" ), Icon( "place" )]
+partial class TestCollectable : Collectable
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+	}
+}
