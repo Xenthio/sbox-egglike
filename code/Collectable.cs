9a029b2770080d97d05daee4bd951d4f3a957cf2
Xenthio <thegooster88@gmail.com>
2022-05-18 09:25:50 +1000

    Create Collectable

---
+++ code/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Hammer;
+
+[Library( "collectable_base" )]
+[Display( Name = "Collectable" ), Category( "Collectables" ), Icon( "place" )]
+// Base collectable, for testing everything is here now, seperate stuff later.
+public partial class Collectable : ModelEntity
+{
+	
+}
+
